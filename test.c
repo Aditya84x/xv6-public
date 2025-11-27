b634086a814e5ba6a099a94655a1e9b133e8779b
Aditya84x <adityagodase84@gmail.com>
2025-11-27 20:23:30 +0000

    temp

--- test.c
+++ test.c
@@ -4,13 +4,13 @@
 #include "user.h"
 
 void userHandler(int signum) {
-    printf(1, "User-defined handler called for usr1 signal %d\n", signum);
+    printf(1, "User-defined handler called for SIGSEGV signal %d\n", signum);
 }
 
 int main(int argc, char **argv) {
-    signal(SIGUSR1, userHandler);
-
-    printf(1, "Process %d: Sending SIGUSR1 to self\n", getpid());
-    kill(getpid(), SIGUSR1);
-    return 0;
+    signal(SIGSEGV, userHandler);
+    int *p = (int*)0x80000000; // Invalid memory access to trigger SIGSEGV
+    int val = *p; // This should cause a segmentation fault
+    printf(1, "Value at invalid address: %d\n", val); // This line should not be reached
+    exit();
 }

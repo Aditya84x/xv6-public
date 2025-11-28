fc4441268c5b61d7d1e772596befb55c8e531a09
ujwal <ujwal56256@gmail.com>
2025-11-28 23:09:41 +0530

    added usertests

--- test.c
+++ test.c
@@ -3,53 +3,207 @@
 #include "stat.h"
 #include "user.h"
 
-// void handler(int sig) {
-//     printf(1,"SIGSEGV caught! sig=%d\n", sig);
-
-//     // Immediately send SIGSEGV to self again → infinite loop
-//     kill(getpid(), SIGSEGV);
-// }
-
-
-void handler(int sig) {
-  printf(1, "Handler: caught SIGINT (%d)\n", sig);
-  sigreturn();
+void handler_int(int sig) {
+  printf(1,"This is from user defined handler for signal %d\n", sig);
 }
 
-int
-main(void)
-{
-  uint oldmask, newmask;
-
-  // Install handler for SIGINT
-  signal(SIGINT, handler);
-
-  printf(1, "Test 1: Blocking SIGINT\n");
-  newmask = (1 << SIGINT);
-
-  sigprocmask(SIG_BLOCK, &newmask, &oldmask);
-
-  printf(1, "Old mask was: 0x%x\n", oldmask);
-  printf(1, "Now SIGINT is blocked. Sending SIGINT...\n");
-
-  kill(getpid(), SIGINT);
-
-  printf(1, "If BLOCK is working, no handler message should appear.\n");
-
-  // Give time for signal (it should not trigger)
-  sleep(100);
-
-  printf(1, "Test 2: Unblocking SIGINT\n");
-  sigprocmask(SIG_UNBLOCK, &newmask, &oldmask);
-  printf(1, "Old mask was: 0x%x\n", oldmask);
-
-  printf(1, "Sending SIGINT again. Handler SHOULD run now.\n");
-  kill(getpid(), SIGINT);
-
-  // Sleep so handler runs
-  sleep(100);
-
-  printf(1, "Test finished.\n");
-
+void handler_segv(int sig) {
+  printf(1,"Hi from segmentation fault handler SIGNO%d\n", sig);
   exit();
 }
+
+void handler_usr1(int sig) {
+  printf(1,"This is user defined handler signal 1 received: %d\n", sig);
+}
+
+void handler_chld(int sig) {
+  printf(1,"This is from child signal handler SIGNO%d\n", sig);
+}
+
+void handler_term(int sig) {
+  printf(1,"This is from termination signal handler SIGNO%d\n", sig);
+}
+
+void handler_cont(int sig) {
+  printf(1,"Hi from continue signal handler SIGNO%d\n", sig);
+}
+
+void handler_ill(int sig) {
+  printf(1,"This is from illegal instruction signal handler SIGNO%d\n", sig);
+}
+
+void handler_vtalrm(int sig) {
+  printf(1,"This is from virtual alarm signal handler SIGNO%d\n", sig);
+}
+
+void test_sigusr1() {
+  printf(1,"BASIC SIGUSR1 TEST\n");
+  signal(SIGUSR1, handler_usr1);
+  kill(getpid(), SIGUSR1);
+  sleep(20);
+  printf(1,"BASIC SIGUSR1 TEST PASSED\n");
+
+}
+
+void test_pause() {
+  printf(1,"PAUSE TEST\n");
+  int pid = fork();
+  if(pid == 0){
+    signal(SIGUSR1, handler_usr1);
+    printf(1, "Pause called by child\n");
+    
+    if(pause() != -1){
+        printf(1, "Pause Failed\n");
+    }
+    
+    printf(1, "Child resumed from pause\n");
+    exit();
+  }
+
+  sleep(20);
+  printf(1, "Parent sending SIGUSR1 to child %d\n", pid);
+  kill(pid, SIGUSR1);
+  
+  wait();
+  printf(1, "Pause Test passed.\n");
+}
+
+void test_sigprocmask() {
+  printf(1,"SIGPROCMASK TEST\n");
+  signal(SIGINT, handler_int);
+
+  uint newmask = (1 << SIGINT);
+  uint oldmask;
+
+  printf(1, "Blocking SIGINT...\n");
+  sigprocmask(SIG_BLOCK, &newmask, &oldmask);
+
+  printf(1, "SIGINT sent while blocked, Handler shouldn't run.\n");
+  kill(getpid(), SIGINT);
+  sleep(50);
+
+  printf(1, "Unblocking SIGINT...\n");
+  sigprocmask(SIG_UNBLOCK, &newmask, &oldmask);
+  printf(1, "Handler should run now if any SIGINT pending.\n");
+  sleep(50);
+
+  printf(1, "SIGINT sent again Handler should run now.\n");
+  kill(getpid(), SIGINT);
+  sleep(50);
+
+  printf(1, "Sigprocmask test done.\n");
+}
+
+void test_sigterm() {
+  printf(1,"SIGTERM TEST\n");
+  signal(SIGTERM, handler_term);
+  printf(1, "Sending SIGTERM.\n");
+  kill(getpid(), SIGTERM);
+  printf(1, "SIGTERM test passed.\n");
+}
+
+void test_sigkill() {
+  printf(1,"SIGKILL TEST\n");
+  int pid = fork();
+  if(pid == 0){
+    signal(SIGKILL, handler_int); // this should not work
+    while(1) {
+      printf(1, "Child running...\n");
+      sleep(20);
+    }
+    exit();
+  }
+
+  sleep(50);
+  printf(1, "Parent sending SIGKILL to child %d\n", pid);
+  kill(pid, SIGKILL);
+  
+  wait();
+  printf(1, "SIGKILL Test passed.\n");
+}
+
+void test_sigill_sigsev_sigvtalrm() {
+  printf(1,"SIGILL, SIGSEGV, SIGVTALRM TEST\n");
+
+  signal(SIGILL, handler_ill);
+  printf(1, "Sending SIGILL.\n");
+  kill(getpid(), SIGILL);
+  sleep(10);
+
+  printf(1, "Forking to test Seg Fault...\n");
+  int pid = fork();
+  if(pid == 0){
+      signal(SIGSEGV, handler_segv);
+      printf(1, "Causing Seg Fault to raise SIGSEGV.\n");
+      int *p = (int*)0x80000000; 
+      *p = 10; 
+  }
+  wait(); //parent waits for child to die from segfault
+
+  signal(SIGVTALRM, handler_vtalrm);
+  printf(1, "Sending SIGVTALRM to self.\n");
+  kill(getpid(), SIGVTALRM);
+  sleep(10);
+
+  printf(1, "SIGILL, SIGSEGV, SIGVTALRM test done.\n");
+}
+
+void test_sigchld() {
+  printf(1,"SIGCHLD TEST\n");
+  signal(SIGCHLD, handler_chld);
+
+  int pid = fork();
+  if(pid == 0){
+    printf(1, "Child process exiting and will raise SIGCHLD in parent.\n");
+    exit();
+  }
+
+  sleep(50); 
+  wait();
+  printf(1, "SIGCHLD test done.\n");
+}
+
+void test_sigstop_sigcont() {
+  printf(1,"SIGSTOP and SIGCONT TEST\n");
+  signal(SIGSTOP, handler_int); // this should not work
+  signal(SIGCONT, handler_cont);
+
+  int pid = fork();
+  if(pid == 0){
+    while(1) {
+      printf(1, "Child running...\n");
+      sleep(20);
+    }
+    exit();
+  }
+
+  sleep(50);
+  printf(1, "Parent sending SIGSTOP to child %d\n", pid);
+  kill(pid, SIGSTOP);
+
+  sleep(100);
+  printf(1, "Parent sending SIGCONT to child %d\n", pid);
+  kill(pid, SIGCONT);
+
+  sleep(50);
+  printf(1, "Parent sending SIGKILL to child %d to end test\n", pid);
+  kill(pid, SIGKILL);
+  
+  wait();
+  printf(1, "SIGSTOP and SIGCONT Test passed.\n");
+}
+
+int main(int argc, char *argv[]) {
+  test_sigusr1();
+  test_pause();
+  test_sigprocmask();
+  test_sigterm();
+  test_sigkill();
+  test_sigchld();
+  test_sigstop_sigcont();
+  test_sigill_sigsev_sigvtalrm();
+  printf(1, "All signal tests completed.\n");
+  printf(1,"OS was good!");
+  exit();
+}
+
